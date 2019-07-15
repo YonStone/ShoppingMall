89bce08cdad523f46ec83379ca321cbc8a14b5da
SmartinUU <1533204193@qq.com>
2019-07-15 15:05:00 +0800

    初始化homeFragment

--- app/src/main/java/com/youdu/shoppingmall/home/fragment/HomeFragment.java
+++ app/src/main/java/com/youdu/shoppingmall/home/fragment/HomeFragment.java
@@ -1,12 +1,18 @@
 package com.youdu.shoppingmall.home.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.youdu.shoppingmall.R;
 import com.youdu.shoppingmall.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author YonStone
@@ -14,22 +20,47 @@ import com.youdu.shoppingmall.base.BaseFragment;
  * @description
  */
 public class HomeFragment extends BaseFragment {
-    private TextView textView;
+
+    @Bind(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @Bind(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @Bind(R.id.rv_home)
+    RecyclerView rvHome;
+    @Bind(R.id.ib_top)
+    ImageButton ibTop;
 
     @Override
     protected View initView() {
-        Log.e(TAG, "主页视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fragment_home, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
-        Log.e(TAG, "主页数据被初始化了");
-        textView.setText("主页");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "信息", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+                rvHome.scrollToPosition(0);
+                break;
+            default:
+        }
     }
 }
