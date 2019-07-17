de9ba818d3b7285964eef0b6dbd346c38aa368d5
SmartinUU <1533204193@qq.com>
2019-07-17 15:47:58 +0800

    商品xml

---
+++ app/src/main/java/com/youdu/shoppingmall/app/GoodsInfoActivity.java
@@ -0,0 +1,15 @@
+package com.youdu.shoppingmall.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.youdu.shoppingmall.R;
+
+public class GoodsInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_info);
+    }
+}
