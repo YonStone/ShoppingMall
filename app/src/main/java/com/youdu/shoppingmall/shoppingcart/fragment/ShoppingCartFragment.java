7bdad200704b4664c76b0b725730c4891443a48f
SmartinUU <1533204193@qq.com>
2019-07-18 15:33:42 +0800

    购物车UI

--- app/src/main/java/com/youdu/shoppingmall/shoppingcart/fragment/ShoppingCartFragment.java
+++ app/src/main/java/com/youdu/shoppingmall/shoppingcart/fragment/ShoppingCartFragment.java
@@ -1,11 +1,9 @@
 package com.youdu.shoppingmall.shoppingcart.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
 
+import com.youdu.shoppingmall.R;
 import com.youdu.shoppingmall.base.BaseFragment;
 import com.youdu.shoppingmall.home.bean.GoodsBean;
 import com.youdu.shoppingmall.shoppingcart.utils.CartProvider;
@@ -18,23 +16,16 @@ import java.util.List;
  * @description
  */
 public class ShoppingCartFragment extends BaseFragment {
-    private TextView textView;
 
     @Override
     protected View initView() {
-        Log.e(TAG, "购物车视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_shoppingcart, null);
+        return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
-        Log.e(TAG, "购物车数据被初始化了");
-        textView.setText("购物车");
 
         List<GoodsBean> goodsBeans = CartProvider.getInstance().getAllData();
         for (int i = 0; i < goodsBeans.size(); i++) {
