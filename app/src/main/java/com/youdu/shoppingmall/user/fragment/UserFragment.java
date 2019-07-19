b165299c09f2f2924c505de068d517f709b3bd03
SmartinUU <1533204193@qq.com>
2019-07-19 10:56:50 +0800

    个人中心UI

--- app/src/main/java/com/youdu/shoppingmall/user/fragment/UserFragment.java
+++ app/src/main/java/com/youdu/shoppingmall/user/fragment/UserFragment.java
@@ -1,12 +1,17 @@
 package com.youdu.shoppingmall.user.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.youdu.shoppingmall.R;
 import com.youdu.shoppingmall.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author YonStone
@@ -14,22 +19,80 @@ import com.youdu.shoppingmall.base.BaseFragment;
  * @description
  */
 public class UserFragment extends BaseFragment {
+    @Bind(R.id.ib_user_icon_avator)
+    ImageButton ibUserIconAvator;
+    @Bind(R.id.tv_username)
+    TextView tvUsername;
+    @Bind(R.id.tv_all_order)
+    TextView tvAllOrder;
+    @Bind(R.id.tv_user_pay)
+    TextView tvUserPay;
+    @Bind(R.id.tv_user_receive)
+    TextView tvUserReceive;
+    @Bind(R.id.tv_user_finish)
+    TextView tvUserFinish;
+    @Bind(R.id.tv_user_drawback)
+    TextView tvUserDrawback;
+    @Bind(R.id.tv_user_location)
+    TextView tvUserLocation;
+    @Bind(R.id.tv_user_collect)
+    TextView tvUserCollect;
+    @Bind(R.id.tv_user_coupon)
+    TextView tvUserCoupon;
+    @Bind(R.id.tv_user_score)
+    TextView tvUserScore;
+    @Bind(R.id.tv_user_prize)
+    TextView tvUserPrize;
+    @Bind(R.id.tv_user_ticket)
+    TextView tvUserTicket;
+    @Bind(R.id.tv_user_invitation)
+    TextView tvUserInvitation;
+    @Bind(R.id.tv_user_callcenter)
+    TextView tvUserCallcenter;
+    @Bind(R.id.tv_user_feedback)
+    TextView tvUserFeedback;
+    @Bind(R.id.scrollview)
+    ScrollView scrollview;
+    @Bind(R.id.tv_usercenter)
+    TextView tvUsercenter;
+    @Bind(R.id.ib_user_setting)
+    ImageButton ibUserSetting;
+    @Bind(R.id.ib_user_message)
+    ImageButton ibUserMessage;
     private TextView textView;
 
     @Override
     protected View initView() {
-        Log.e(TAG, "个人视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_user, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
-        Log.e(TAG, "个人数据被初始化了");
-        textView.setText("个人");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.ib_user_icon_avator, R.id.ib_user_setting, R.id.ib_user_message})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ib_user_icon_avator:
+//                Intent intent = new Intent(mContext, LoginActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.ib_user_setting:
+                Toast.makeText(mContext, "设置", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_user_message:
+//                Intent intent = new Intent(mContext, MessageCenterActivity.class);
+//                startActivity(intent);
+                break;
+        }
     }
 }
